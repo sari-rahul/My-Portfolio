f37db3e957e3f8ad4ad6e0d9bc4e435abc5cff65
sari-rahul <sarisivadas93@gmail.com>
2024-05-31 19:51:53 +0200

    Complete Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from '../src/components/NavBar';
+import { Container } from 'react-bootstrap';
+import { Routes, Route } from 'react-router-dom';
+import Portfolio from '../src/pages/Portfolio';
+import About from '../src/pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Container>
+          <Routes>
+            <Route path="/" element={<Portfolio />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Container } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
+import Logo from '../assets/logo.png';
+import styles from '../styles/NavBar.module.css';
+
+const NavBar = () => {
+  const location = useLocation();
+
+  return (
+    <Navbar expand="md" data-bs-theme="light" className={styles.NavBar}>
+      <Container className={styles.NavContainer}>
+        <Navbar.Brand as={Link} to="/about">
+            <img src={Logo} alt="Logo" height={70} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className={styles.ToggleButton}/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <div className={styles.NavLinks}>
+            <Nav.Item>
+              <Link to="/about" className={`${styles.NavLink} ${location.pathname === '/about' ? styles.Active : ''}`}>
+                about
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link to="/portfolio" className={`${styles.NavLink} ${location.pathname === '/portfolio' ? styles.Active : ''}`}>
+                portfolio
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link to="/contact" className={`${styles.NavLink} ${location.pathname === '/contact' ? styles.Active : ''}`}>
+                contact
+              </Link>
+            </Nav.Item>
+            </div>
+            <div className={styles.NavLinks}>
+              <Nav.Link href="#">
+                <i className="fa-brands fa-facebook"></i>
+              </Nav.Link>
+              <Nav.Link href="#">
+                <i className="fa-brands fa-instagram"></i>
+              </Nav.Link>
+              <Nav.Link href="#">
+                <i className="fa-brands fa-linkedin"></i>
+              </Nav.Link>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+};
+
+export default NavBar;
