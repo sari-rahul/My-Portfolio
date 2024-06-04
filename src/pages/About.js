84115dd231334eadebbb0a17b68af3051a06c11c
sari-rahul <sarisivadas93@gmail.com>
2024-06-04 15:32:31 +0200

    Edit About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,68 +2,76 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import styles from '../styles/About.module.css';
 import SkillsChart from '../components/Skills';
-const About = () => {
+import DeskImage  from '../assets/desktopimage.jpg';
+import FactsImage from '../assets/momAndChildGarndening.jpg';
+import PortfolioImage from '../assets/github-image.png';
+
+const Aboutnew = () => {
   return (
     <>
     <Container className={styles.AboutContainer}>
-      <div className={styles.LeftContainer}>
-        <div className={styles.Heading}>about.</div>
-        <div className={styles.SubHeading}>
-          I’m a web developer based in vibrant Stuttgart, Germany.
+        <div className={`${styles.LeftContainer} text-start }`}>
+            <div className={styles.BigHeading}>about.</div>
+            <div className={styles.Title}>
+                I’m a web developer based in vibrant Stuttgart, Germany.
+            </div>
+            <div className={styles.Details}>
+            Since 2019, I've been dedicated to transforming complex 
+            challenges into elegant, efficient, and user-friendly web 
+            solutions. When I'm not immersed in code, you can find me 
+            experimenting with new recipes in the kitchen, gardening 
+            or enjoying outdoors.
+            </div>
         </div>
-        <div className={styles.Data}>
-        Since 2019, I've been dedicated to transforming complex 
-        challenges into elegant, efficient, and user-friendly web 
-        solutions. When I'm not immersed in code, you can find me 
-        experimenting with new recipes in the kitchen, gardening 
-        or enjoying outdoors.
+        <div className={styles.RightContainer}>
+            <img src={PortfolioImage} alt="portfolio" />
         </div>
-      </div>
-      <div className={styles.RightContainer}>
-        <img src="" alt="" />
-      </div>
     </Container>
-    <div className={`${styles.AboutContainer} ${styles.GreyBackground}`}>
-      <div className={styles.RandomLeft}>
-        <img src="" alt="" />
-      </div>
-      <div className={styles.RandomRight}>
-          <div className={styles.RandomHeading}>Random Facts</div>
-          <div className={styles.Data}>
-            <ul>I am a Coffee Person.</ul>
-            <ul>I am into designing and sketching.</ul>
-            <ul>I love cooking and trying new recipes.</ul>
-            <ul>Gardening is my zen time.</ul>
-            <ul>I'm a bit of a clean freak.</ul>
-            <ul>A proud and loving mother.</ul>
-            <ul>Love spending quality time with my family.</ul>
-          </div>
-      </div>
-    </div>
-    <Container className={styles.SkillContainer}>
-        <h1> My Skills</h1>
+    <div className={`${styles.GreyBackground}  p-5`}>
+        <Container>
+        <h1 className= {`${styles.Heading} text-end`}>My Skills</h1>
         <SkillsChart/>
-    </Container>
-    <div className={` ${styles.GreyBackground}`}>
-      <div className={styles.InnerContainer}>
-        <div className={`${styles.MystoryLeft} ${styles.SmallContainer}`}>
-          <div className={`${styles.RandomHeading} text-start p-0`}>My Story</div>
-          <div className={`${styles.Data} text-start ml-auto`}>
-            Learn a little bit more about me, how I got into developing , and how 
-            I built my career as a Web Developer. I’ve included key things 
-            I've learned, book recommendations, and even some sneak peeks of the 
-            first websites I created.
-          </div>
-          <br />
-          <button className={`${styles.ReadmeButton} text-start`}>Read My Story</button>
-        </div>
-        <div className={styles.MystoryRight}>
-          <img src="" alt="" />
-        </div>
-      </div>
+        </Container>
     </div>
-    </>
-  );
-};
+    <Container className={styles.AboutContainer}>
+        <div className={styles.LeftContainer}>
+            <img src={FactsImage} alt="Mom and son in garden" />
+        </div>
+        <div className={`${styles.RightContainer} ${styles.Height} ${styles.PaddingLeft} pt-5  text-start`}>
+            <div className={styles.Heading}>Random Facts</div>
+            <div className={`${styles.Details}`}>
+                <ul>I am a Coffee Person.</ul>
+                <ul>I am into designing and sketching.</ul>
+                <ul>I love cooking and trying new recipes.</ul>
+                <ul>Gardening is my zen time.</ul>
+                <ul>I'm a bit of a clean freak.</ul>
+                <ul>A proud and loving mother.</ul>
+                <ul>Love spending quality time with my family.</ul>    
+            </div>
+        </div>
+    </Container>
+    <div className={styles.GreyBackground}>
+        <Container className={styles.AboutContainer}>
+            <div className={`${styles.LeftContainer} text-start }`}>
+                <div className={`${styles.Heading} p-0`}>My Story</div>
+                <div className={`${styles.Details}`}>
+                    Learn a little bit more about me, how I got into developing , and how 
+                    I built my career as a Web Developer. I’ve included key things 
+                    I've learned, book recommendations, and even some sneak peeks of the 
+                    first websites I created.
+                </div>
+                <br />
+                <button className={`${styles.ReadmeButton} text-start`}>Read my story</button>
 
-export default About
+            </div>
+            <div className={`${styles.RightContainer} ${styles.Height} `}>
+                <img src={DeskImage} alt="workstation" className={`${styles.Border} w-75 h-75`} />
+            </div>
+        </Container>
+    </div>
+      
+    </>
+  )
+}
+
+export default Aboutnew
